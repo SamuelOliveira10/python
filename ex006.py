6ded9e1f78439d66f25ea8e3c149db159bbeb478
SamuelOliveira10 <samueloliveirafn7@gmail.com>
2024-03-04 20:59:04 -0300

    fiz ex005 e 006

---
+++ ex006.py
@@ -0,0 +1,8 @@
+from math import sqrt
+
+num = int(input('Digite um número: '))
+print(f"""
+O dobro de {num} vale {num*2}
+O triplo de {num} vale {num*3}
+A raiz quadrada de {num} é {sqrt(num)}
+""")
